6b0a36943a7d6fe3e7ddf759977b0ba28cb2ba01
Grace Melton <gmelton@udel.edu>
2023-05-12 21:39:14 -0400

    completed exercise 2

---
+++ Exercise2/schedulers.c
@@ -0,0 +1,108 @@
+#include<stdio.h>
+#include <stdbool.h>
+int main()
+{
+// Initialize variables
+int i = 0, num_processes = 0, total_time = 0, x, output_flag = 0,
+time_quantum =0;
+int wait_time = 0, turnaround_time = 0, arrival_time[10], burst_time[10],
+temp[10];
+float average_wait_time = 0, average_turnaround_time = 0;
+bool round_robin_algorithm = false;
+for(i = 0; i < 10; i++)
+ {
+arrival_time[i] = 0;
+burst_time[i] = 0;
+temp[i] = 0;
+ }
+// Define Number of Processes
+while(num_processes < 1 || num_processes > 10)
+ {
+printf("\nEnter Total Number of Processes (1 - 10):\t");
+scanf("%d", &num_processes);
+if (num_processes < 1 || num_processes > 10)
+ {
+printf("Incorrect Value Entered");
+ }
+ }
+// Define Process Details
+x = num_processes;
+for(i = 0; i < num_processes; i++)
+ {
+printf("\nEnter Details of Process[%d]\n", i + 1);
+printf("Arrival Time (>-1):\t");
+scanf("%d", &arrival_time[i]);
+printf("Burst Time (>0):\t");
+scanf("%d", &burst_time[i]);
+temp[i] = burst_time[i];
+if (arrival_time[i] < 0 && burst_time[i] < 1) {
+printf("Incorrect Values Entered");
+i--;
+ }
+ }
+// Define Quantum
+if (round_robin_algorithm)
+ {
+while(time_quantum < 1)
+ {
+printf("\nEnter Time Quantum (>0):\t");
+scanf("%d", &time_quantum);
+if (time_quantum < 1)
+ {
+printf("Incorrect Value Entered");
+ }
+ }
+ }
+// Prepare Output
+printf("\nProcess ID\t\tArrival Time\t\tBurst Time\t Turnaround Time\t Waiting Time\n");
+// Perform Scheduling Calculations
+if (!round_robin_algorithm)
+ {
+    for(int i = 0; i <num_processes -1; i++){
+        for(int j= i + 1; j < num_processes; j++){
+            if(arrival_time[j] < arrival_time[i]){
+                int tmp_arrive  = arrival_time[i];
+                arrival_time[i]= arrival_time[j];
+                arrival_time[j] = tmp_arrive;
+
+                int tmp_burst = burst_time[i];
+                burst_time[i] = burst_time[j];
+                burst_time[j] = tmp_burst;
+
+                int tmp = temp[i];
+                temp[i] = temp[j];
+                temp[j] = tmp;
+            }
+        }
+    }
+ }
+
+// Calculate & Print Average Wait and Turnaround Times
+average_wait_time = 0;
+average_turnaround_time = 0;
+
+for(i = 0; i < num_processes; i++){
+    if(i == 0){
+        wait_time = 0;
+    }
+    else{
+        wait_time = total_time - arrival_time[i];
+    }
+
+    turnaround_time = wait_time + burst_time[i];
+
+    total_time += burst_time[i];
+
+    printf("\n%d\t\t\t%d\t\t\t%d\t\t\t%d\t\t\t%d", i + 1, arrival_time[i], burst_time[i], turnaround_time, wait_time);
+
+    average_wait_time += wait_time;
+    average_turnaround_time += turnaround_time;
+}
+
+average_wait_time = average_wait_time/num_processes;
+average_turnaround_time = average_turnaround_time/num_processes;
+
+printf("\n\nAverage Waiting Time:\t%f", average_wait_time);
+printf("\nAvg Turnaround Time:\t%f\n", average_turnaround_time);
+return 0;
+}
